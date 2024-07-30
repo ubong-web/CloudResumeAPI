91196282f5841487aad1f3905e903ed95a8395cc
John Ubong <ubongj368@gmail.com>
2024-07-30 11:27:44 +0100

    updated

--- website/assets/js/index.js
+++ website/assets/js/index.js
@@ -5,7 +5,7 @@ const counter = document.querySelector('.p_view');
 
 
 async function updateCounter() {
-    const response = await fetch("https://bvwalakr562en5cqcvoyyhwuxa0rfugq.lambda-url.us-east-1.on.aws/");
+    const response = await fetch("https://bvwalakr562en5cqcvoyyhwuxa0rfugq.lambda-url.us-east-1.on.aws");
     const data = await response.json();
     
     counter.textContent = `Page Views: ${data}`
